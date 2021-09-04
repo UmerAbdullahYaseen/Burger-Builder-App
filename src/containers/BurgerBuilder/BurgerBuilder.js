ae124bdcdbffaf5ca49e96152e388b63b78e49c3
Umer Abdullah Yaseen <umerabdillah6223@gmail.com>
2021-09-04 16:05:26 +0500

    first commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxiliry';
+
+class BurgerBuilder extends Component {
+render(){
+    
+     return(
+         
+             <Aux>
+                 <div>Burger</div>            
+                 <div>Build Control</div> 
+             </Aux>
+
+     );
+
+
+
+}
+
+
+
+}
+
+export default BurgerBuilder;
